6611c44eb9bd8b88abf7d6ba922f32ff6b32b14d
hawk86104 <hawk86104@163.com>
2025-08-20 10:23:24 +0800

    no message

---
+++ js/DevTDTTiles.vue_vue_type_script_setup_true_lang.CD8DT10R1755656106911.js
@@ -0,0 +1,1 @@
+import{K as e}from"./@icegl.BJ9bxFz61755656106911.js";import{d as a,e as t,o as r,u as s}from"./@vue.BzMISRrV1755656106911.js";const l=a({__name:"DevTDTTiles",props:{layer:{type:String,default:"vec"}},setup:a=>(l,o)=>(r(),t(s(e),{layer:a.layer,url:s("https://t0.tianditu.gov.cn"),tk:"890a908c5d3f6c77232b60964d74f180"},null,8,["layer","url"]))});export{l as _};
