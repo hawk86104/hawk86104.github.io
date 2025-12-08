f8c8abf303736207bd39320246602d302c09f9d2
hawk86104 <hawk86104@163.com>
2025-12-08 17:35:36 +0800

    no message

---
+++ js/utils.DN2a2PyE1765186091469.js
@@ -0,0 +1,1 @@
+import{R as e}from"./three-stdlib.D7QQQolC1765186091469.js";import{r,a9 as s}from"./three.F31Lz30M1765186091469.js";const t=t=>new Promise((a,i)=>{(new e).load(t,(e,t)=>{e.minFilter=r,e.magFilter=r,e.mapping=s,e.needsUpdate=!0,a(e)})});export{t as l};
