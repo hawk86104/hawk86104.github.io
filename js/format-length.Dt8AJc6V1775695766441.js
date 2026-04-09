790082a4c396c0be29d24ebe02d0751c310fe5db
hawk86104 <hawk86104@users.noreply.github.com>
2026-04-09 00:52:36 +0000

    deploy: auto publish from three-vue-tres hawk86104/three-vue-tres@1f20e35f0cd40890647ad3afa57473f0ed3d2282

---
+++ js/format-length.Dt8AJc6V1775695766441.js
@@ -0,0 +1,1 @@
+const t=/^(\d|\.)+$/,e=/(\d|\.)+/;function r(r,{c:n=1,offset:c=0,attachPx:o=!0}={}){if("number"==typeof r){const t=(r+c)*n;return 0===t?"0":`${t}px`}if("string"==typeof r){if(t.test(r)){const t=(Number(r)+c)*n;return o?0===t?"0":`${t}px`:`${t}`}{const t=e.exec(r);return t?r.replace(e,String((Number(t[0])+c)*n)):r}}return r}export{r as formatLength};
